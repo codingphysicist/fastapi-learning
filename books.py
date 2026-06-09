1196d7cb7e6675724032fb9e0aa49740b60bc8fb
coding physicist <aman.work121@gmail.com>
2026-06-09 15:02:31 +0530

    added path parameter

--- books.py
+++ books.py
@@ -16,4 +16,8 @@ BOOKS = [
 async def read_all_books() -> list:
     return BOOKS
 
-
+@app.get("/books/{book_title}")
+async def get_book(book_title:str):
+    for book in BOOKS:
+        if book.get("title").casefold()==book_title.casefold():
+            return book
