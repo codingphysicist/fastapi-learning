d3dea5d75b2ad0ac9190d9613a1a8dd16e831e24
coding physicist <aman.work121@gmail.com>
2026-06-14 23:43:30 +0530

    added explicit status code

--- book2.py
+++ book2.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI,Path, Query, HTTPException
 from pydantic import BaseModel, Field
-
+from starlette import status
 app= FastAPI()
 
 
@@ -51,7 +51,7 @@ BOOKS=[
     Book(6, "Beyond the Basic Stuff with Python", "Al Sweigart", "Intermediate Python concepts for improving coding skills.", 2,2010)
 ]
 
-@app.get("/book/{book_id}")
+@app.get("/book/{book_id}",status_code=status.HTTP_200_OK)
 async def get_specific_book(book_id:int=Path(gt=0)):
     for book in BOOKS:
         if book.id==book_id:
@@ -66,11 +66,11 @@ async def book_by_rating(book_rating:int=Query(gt=0,lt=6)):
             books_to_return.append(book)
     return books_to_return
 
-@app.get("/books")
+@app.get("/books",status_code=status.HTTP_200_OK)
 async def read_all_books():
     return BOOKS
 
-@app.post("/books/create-book")
+@app.post("/books/create-book",status_code=status.HTTP_201_CREATED)
 async def create_book(book_request:BookRequest):
     new_book=Book(**book_request.model_dump())
     BOOKS.append(find_book_id(new_book))
@@ -82,7 +82,7 @@ def find_book_id(book:Book):
         book.id=1
     return book
 
-@app.put("/books/update_book")
+@app.put("/books/update_book",status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book:BookRequest):
     bookchange=False
     for i in range(len(BOOKS)):
@@ -94,7 +94,7 @@ async def update_book(book:BookRequest):
 
 
 
-@app.get("/books/publish")
+@app.get("/books/publish",status_code=status.HTTP_200_OK)
 async def book_by_published_date(published_date:int=Query(gt=1999,lt=2031)):
     books_to_return=[]
     for book in BOOKS:
@@ -103,8 +103,8 @@ async def book_by_published_date(published_date:int=Query(gt=1999,lt=2031)):
     return books_to_return
 
 
-@app.delete("/books/delete_book")
-async def delete_book(book_id:int=Path(gt=0)):
+@app.delete("/books/delete_book",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id:int=Query(gt=0)):
     book_changed=False
     for i in range(len(BOOKS)):
         if BOOKS[i].id==book_id:
