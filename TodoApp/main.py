3899d1c8499df6a7f2c3de722481a1fb03948906
coding physicist <aman.work121@gmail.com>
2026-06-22 02:03:33 +0530

    implemented todo by id

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import Session
 from typing import Annotated
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 import models
+from starlette import status
 from models import Todos
 from database import engine, SessionLocal
 
@@ -16,6 +17,16 @@ def get_db():
     finally:
         db.close()
 
+db_dependency=Annotated[Session,Depends(get_db)]
+
+
 @app.get("/")
-async def read_all(db:Annotated[Session, Depends(get_db)]):
+async def read_all(db:db_dependency):
     return db.query(Todos).all()
+
+@app.get('/todo/{todo_id}',status_code=status.HTTP_200_OK)
+async def read_todo(db:db_dependency,todo_id:int=Path(gt=0)):
+    todo_model=db.query(Todos).filter(Todos.id==todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404,detail="Todo not found with that id")
