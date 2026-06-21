4117c76653cd5dc39dbcef84cfbabbbe4f09b869
coding physicist <aman.work121@gmail.com>
2026-06-22 02:37:33 +0530

    implemented create_todo

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -5,7 +5,7 @@ import models
 from starlette import status
 from models import Todos
 from database import engine, SessionLocal
-
+from pydantic import BaseModel, Field
 app=FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
@@ -20,6 +20,13 @@ def get_db():
 db_dependency=Annotated[Session,Depends(get_db)]
 
 
+class TodoRequest(BaseModel):
+    title:str =Field(min_length=3)
+    description: str=Field(min_length=3, max_length=100)
+    priority:int=Field(gt=0,lt=6)
+    complete:bool
+
+
 @app.get("/")
 async def read_all(db:db_dependency):
     return db.query(Todos).all()
@@ -30,3 +37,10 @@ async def read_todo(db:db_dependency,todo_id:int=Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404,detail="Todo not found with that id")
+
+
+@app.post("/todo",status_code=status.HTTP_201_CREATED)
+async def create_todo(db:db_dependency,todo_request:TodoRequest):
+    todo_model=Todos(**todo_request.model_dump())
+    db.add(todo_model)
+    db.commit()
